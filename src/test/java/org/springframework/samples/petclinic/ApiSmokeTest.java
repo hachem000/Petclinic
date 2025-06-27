8e9f4ba77c1ac4fed4ce33e4aa36936c7e11687a
hachem000 <76076094+hachem000@users.noreply.github.com>
2025-06-27 13:56:45 +0000

    smok test

--- src/test/java/org/springframework/samples/petclinic/ApiSmokeTest.java
+++ src/test/java/org/springframework/samples/petclinic/ApiSmokeTest.java
@@ -8,15 +8,11 @@ import static org.hamcrest.Matchers.*;
 
 public class ApiSmokeTest {
 
-    @Test
-    public void testHomePageIsUp() {
-        RestAssured.baseURI = "https://petclinic.onrender.com";
+	@Test
+	public void testHomePageIsUp() {
+		RestAssured.baseURI = "https://petclinic.onrender.com";
 
-        given().
-        when().
-            get("/").
-        then().
-            statusCode(200).
-            body(containsString("Welcome"));
-    }
+		given().when().get("/").then().statusCode(200).body(containsString("Welcome"));
+	}
+
 }
