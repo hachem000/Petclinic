5c9d3fc7de64eca07bae74e5af53e50d468352f8
Smash OZEOL <smash@ozeol.com>
2025-07-08 13:24:22 +0100

    Added Trivy image scan

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -50,9 +50,6 @@ public class Person extends BaseEntity {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	String password = "admin123"; // hardcoded secret
-	public String getPassword() {
-		return password;
-	}
+	
 
 }
