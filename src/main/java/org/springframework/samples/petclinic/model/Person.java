b213fe448f50b882fe437961f2f9be4bf39aaedf
Smash OZEOL <smash@ozeol.com>
2025-07-08 11:30:24 +0100

    added intentional hard coded secret

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -50,5 +50,9 @@ public class Person extends BaseEntity {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	String password = "admin123"; // hardcoded secret
+	public String getPassword() {
+		return password;
+	}
 
 }
